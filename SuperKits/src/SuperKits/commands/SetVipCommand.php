d523d170c233acc4d304fad5d4ed5d5bdb3b6065
TanneriOS <txnnerios@gmail.com>
2016-07-14 15:00:15 -0400

    Update SetVipCommand.php

--- SuperKits/src/SuperKits/commands/SetVipCommand.php
+++ SuperKits/src/SuperKits/commands/SetVipCommand.php
@@ -1,3 +1,36 @@
 <?php
 
 namespace SuperKits\commands;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\Plugin;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExecutor;
+
+use SuperKits\Main;
+
+class SetVipCommand extends PluginBase() {
+  
+  /** @var Main */
+  private $plugin;
+  
+  /** @return Main */
+  public function __construct(Main $plugin) {
+    $this->plugin = $plugin;
+  }
+  
+  /** @return Plugin */
+  public function getPlugin() {
+    return $this->plugin;
+  }
+  
+  public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+    $name = $sender->getName();
+    $vip = new Config($this->plugin->getDataFolder() . "vip/vip.json", Config::JSON);
+  }
+}
