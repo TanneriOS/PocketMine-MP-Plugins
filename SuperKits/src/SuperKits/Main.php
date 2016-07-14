785e149ddddf1c812e930e88cf14c52da50f52e4
TanneriOS <txnnerios@gmail.com>
2016-07-14 14:45:05 -0400

    Update Main.php

--- SuperKits/src/SuperKits/Main.php
+++ SuperKits/src/SuperKits/Main.php
@@ -6,9 +6,15 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 use pocketmine\utils\Config;
 
+use SuperKits\commands\KitCommand;
+
 class Main extends PluginBase {
 
   public function onEnable() {
     $this->getLogger()->info("SuperKits Enabled!");
+  }
+  
+  public function setCommands() {
+    $this->getCommand("kit")->setExecutor(new KitCommand($this), $this);
   }
 }
