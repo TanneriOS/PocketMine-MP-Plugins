84f5c415d2d082f29aae070f601a92360d3d9c30
TanneriOS <txnnerios@gmail.com>
2016-07-14 15:27:17 -0400

    Update KitCommand.php

--- SuperKits/src/SuperKits/commands/KitCommand.php
+++ SuperKits/src/SuperKits/commands/KitCommand.php
@@ -30,6 +30,20 @@ class KitCommand extends PluginBase() {
   }
   
   public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
-    
+    $name = $sender->getName();
+    $vip = new Config($this->plugin->getDataFolder() . "vip/vip.json", Config::JSON);
+    $vip+ = new Config($this->plugin->getDataFolder() . "vip+/vip+.json", Config::JSON);
+    switch($command->getName()) {
+      case "kit":
+        if(!(isset($args[0]))) {
+          $sender->sendMessage(TF::RED . "Usage: /kit select <starter|warrior|knight|hunter");
+        }
+        if(isset($args[0])) {
+          if($args[0] == "select") {
+            //TODO: Kits
+          }
+        }
+        break;
+    }
   }
 }
