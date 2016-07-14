5fc3789dd75fc5322a5dfd1b3c1b590fcf64ebb5
TanneriOS <txnnerios@gmail.com>
2016-07-14 15:11:00 -0400

    Update SetVipCommand.php

--- SuperKits/src/SuperKits/commands/SetVipCommand.php
+++ SuperKits/src/SuperKits/commands/SetVipCommand.php
@@ -32,5 +32,25 @@ class SetVipCommand extends PluginBase() {
   public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
     $name = $sender->getName();
     $vip = new Config($this->plugin->getDataFolder() . "vip/vip.json", Config::JSON);
+    if($sender->isOp()) {
+      switch($command->getName()) {
+        case "vip":
+          if(!(isset($args[0]))) {
+            $sender->sendMessage(TF::RED . "Usage: /vip <add|remove> <name>");
+          }
+          if(isset($args[0])) {
+            if($args[0] == "add") {
+              $player = $this->getServer()->getPlayer($args[1]);
+              $vip->set($player, "vip");
+              $sender->sendMessage(TF::GREEN . $player . " is now added to the vip list!");
+            } elseif($args[0] == "remove") {
+              $player = $this->getServer()->getPlayer($args[1]);
+              $vip->set($player, null);
+              $sender->sendMessage(TF::BLUE . $player . " has now been removed from vip list!");
+            }
+          }
+          break;
+      }
+    }
   }
 }
